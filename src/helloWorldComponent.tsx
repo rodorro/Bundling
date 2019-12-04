1a8c3ae177b80e0a73e7c0f57a28389f2631102b
illano <illano@ecna.es>
2019-12-04 12:42:10 +0100

    Exercise Bundling webpack

--- src/helloWorldComponent.tsx
+++ src/helloWorldComponent.tsx
@@ -4,10 +4,12 @@ import { getEnvironmentMode, getApiBase } from './helloworld';
 
 export const HelloWorldComponent: React.FunctionComponent = () => {
   return (
-    <div>
-      <span className={classes.resultBackground}>Hello World!!!</span>
-      <span>ENVIRONMENT MODE: {getEnvironmentMode()}</span>
-      <span>API BASE: {getApiBase()}</span>
+    <div className={classes.helloWorldDiv}>
+      <span className={classes.helloWorldTitle}>Hello World!!!</span>
+      <div className={classes.helloWorldEnvironment}>
+        <span>ENVIRONMENT MODE: {getEnvironmentMode()}</span>
+        <span>API BASE: {getApiBase()}</span>
+      </div>
     </div>
   );
 };
